3ebcdb836e60eefca32075e7a1b3f2f60343d524
Mauldy Putra <mauldyputra@gmail.com>
2020-12-30 10:40:04 +0700

    Initial Commit

---
+++ MyCoreAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCoreAnimation
+//
+//  Created by Mauldy Putra on 30/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
